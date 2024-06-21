6d592574996d5e1adb9698fcc5d2d6ccc6035d84
Shoubhit <Shobthebob@users.noreply.github.com>
2024-06-21 01:53:20 -0400

    Beta version 0.0.1 WORKS!!!!!!

--- extension.js
+++ extension.js
@@ -1,34 +1,80 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/* vs code API provides
+		- 'context' (line::14,17; object)
+		- subscriptions (line::33; array)
+*/
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+// function activate(context) {
+// 	console.log('Congratulations, your extension "to-do-list" is now active!');
+// 	vscode.window.registerWebviewViewProvider("myView", )
+// }
+
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "to-do-list" is now active!');
+	//context is basically the main parent variable that handles everything from the high lvl perspective. It is a variable made by the vs code extention api itself.
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('to-do-list.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// disposables are those objects that have to be freed from the memory once the extention is deactivated
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from To-Do List!');
+	const provider = new MyWebviewViewProvider(context.extensionUri);
+
+	let disposable = vscode.window.registerWebviewViewProvider("toDo", provider, { webviewOptions: { retainContextWhenHidden: true } });
+
+	let disposableCommand = vscode.commands.registerCommand('extension.sayHello', () => {
+		vscode.window.showInformationMessage("YOOOOOOOOOOOOOO!");
 	});
 
+	// Add to context subscriptions
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableCommand);
+}
+
+exports.activate = activate;
+
+class MyWebviewViewProvider {
+	constructor(_extensionUri) {
+		this._extensionUri = _extensionUri;
+	}
+
+	resolveWebviewView(webviewView, context, _token) {
+		this._view = webviewView;
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+		// Set the HTML content for the webview
+		webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+	}
+
+	getHtmlForWebview(webview) {
+		return `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Webview</title>
+            </head>
+            <body>
+                <h1>Hello, Webview!</h1>
+            </body>
+            </html>
+        `;
+	}
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
