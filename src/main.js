9b805c69ef50cf7d3bc16ad43433151306710e05
shobthebob <sxj5604@psu.edu>
2025-04-08 18:09:07 -0400

    made changes to the UI

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     editing: `<svg class="editing-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM192,108.68,147.31,64l24-24L216,84.68Z"></path></svg>`,
     pin: `<svg class="pin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M235.32,81.37,174.63,20.69a16,16,0,0,0-22.63,0L98.37,74.49c-10.66-3.34-35-7.37-60.4,13.14a16,16,0,0,0-1.29,23.78L85,159.71,42.34,202.34a8,8,0,0,0,11.32,11.32L96.29,171l48.29,48.29A16,16,0,0,0,155.9,224c.38,0,.75,0,1.13,0a15.93,15.93,0,0,0,11.64-6.33c19.64-26.1,17.75-47.32,13.19-60L235.33,104A16,16,0,0,0,235.32,81.37ZM224,92.69h0l-57.27,57.46a8,8,0,0,0-1.49,9.22c9.46,18.93-1.8,38.59-9.34,48.62L48,100.08c12.08-9.74,23.64-12.31,32.48-12.31A40.13,40.13,0,0,1,96.81,91a8,8,0,0,0,9.25-1.51L163.32,32,224,92.68Z"></path></svg>`,
     pinned: `<svg class="pinned-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M224,176a8,8,0,0,1-8,8H136v56a8,8,0,0,1-16,0V184H40a8,8,0,0,1,0-16h9.29L70.46,48H64a8,8,0,0,1,0-16H192a8,8,0,0,1,0,16h-6.46l21.17,120H216A8,8,0,0,1,224,176Z"></path></svg>`,
-    delete: `<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg>`
+    delete: `<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg>`,
+    add: `<svg class="add-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path></svg>`
   };
 
   let tasks = []; // with the HTML DOM element
@@ -21,6 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let completedCount = 0;
   const vscode = acquireVsCodeApi();
   let taskCounter;
+
+  addTaskButton.innerHTML = icons.add;
 
   function displayLists() {
     // Used in debugging
@@ -43,6 +46,20 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       element.style.overflowY = 'hidden';
     }
+  }
+
+  function adjustMarginForScrollbar() {
+    const hasScrollbar = todoList.scrollHeight > todoList.clientHeight; // Check if scrollbar is present
+    const listItems = todoList.querySelectorAll('li');
+    
+    listItems.forEach((item) => {
+      if (hasScrollbar) {
+        item.style.marginRight = '5px';
+      } 
+      else {
+        item.style.marginRight = '0';
+      }
+    });
   }
 
   function reindex() {
@@ -102,6 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
     todoList.appendChild(taskItem);
     const taskTextElement = taskItem.querySelector('.task-txt');
     applyScrollIfNeeded(taskTextElement);
+    adjustMarginForScrollbar();
 
     taskTask['element'] = taskItem;
     taskTask['text'] = taskText;
@@ -154,6 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
       vscode.postMessage({ command: 'updateWorkspaceState', tasks: workspaceTasks });
     }
     todoList.removeChild(taskItem);
+    adjustMarginForScrollbar();
 
     if (pinnedCount === 0 & completedCount === 0) {
       reindex();
